cfd6e4e6c142503bc0ccd2ab5ca96a6393fa053b
jagdeep113 <singhjagdeep113@gmail.com>
2024-09-20 23:37:58 +0530

    contants

---
+++ constant/index.php
@@ -0,0 +1,21 @@
+<?php
+// define('PI',3.14,false);
+//define('name',value)
+//const PI=3.14;
+// define("PI",4);
+
+// const PI=3.14;
+
+// const PI=5;
+
+// echo PI;
+// if(true){
+//     define('PI',3.14);
+// }
+// echo PI;
+
+class Exa {
+    const name=[1,2,3];
+}
+ echo Exa::name[2];
+?>
