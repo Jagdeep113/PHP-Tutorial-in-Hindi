780555abf4c950a691a56807de28a93613d1b807
jagdeep113 <singhjagdeep113@gmail.com>
2024-09-26 00:22:26 +0530

    operators

---
+++ opertaors/index.php
@@ -0,0 +1,62 @@
+<?php
+// $a=8;
+// $b=5;
+// // arhtmetic
+// $add=$a+$b;
+// $subtract=$a-$b;
+// $div=$a/$b;
+// $mod=$a%$b;
+// echo " Mod of a and b ".$mod;
+
+// $a=5;
+// $b=5;
+
+// if($a==5 xor $b==6 ){
+//     echo "true";
+// }else{
+//     echo "false";
+// }
+
+// $a=5;
+
+// echo " intial value of a is ".$a--." After Post decrement value is ". $a;
+
+// $str="Hi";
+// //$str=$str." Jagdeep";
+// $str.=" Jagdeep";
+// echo $str;
+
+// $a=['one'=>1,"two"=>2];
+// $b=['three'=>3,"four"=>4];
+
+// // if($a[0] == $b[0])
+// echo $a['one'];
+
+// $a=6;
+// $b=($a==6)? 6:5;
+// echo $b;
+
+// $a=6;
+// $b=($a==5)?? 1;
+// echo $b;
+
+//$a<<$b; //multiply by 2
+//$a >> $b; //divide by 2
+// $a=4;
+//$result=$a<<2;
+// $a=16;
+
+// $result=$a >> 2;
+// echo $result;
+
+// $a=5;
+// $b=14;
+
+// $result=$a <=> $b/2;
+
+// echo $result;
+
+// echo `ls -al`;
+
+// echo @$a;
+?>
