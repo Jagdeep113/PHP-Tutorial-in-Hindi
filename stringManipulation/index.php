70022580c8eb00269e92ef34804ddee3bd16ff2c
jagdeep113 <singhjagdeep113@gmail.com>
2025-12-26 00:42:18 +0530

    php string manipulation

---
+++ stringManipulation/index.php
@@ -0,0 +1,40 @@
+<?php 
+ //$name='jagdeep 
+// hfhhj';
+
+// $str="hello  
+// $name";
+
+
+// echo ucwords($str);
+// $heredoc = <<<EOD
+// This is a HEREDOC string.
+// It spans multiple lines.
+// You can embed variables like $name here.
+// And escape sequences like \n for newlines.
+// EOD;
+// echo $heredoc;
+
+// $nowdoc = <<<'EOD'
+// This is a NOWDOC string.
+// It spans multiple lines.
+// Variables like $name are not expanded.
+// Escape sequences like \n are literal.
+// EOD;
+
+// $name='php';
+ //$str="hi there ";
+// $str.=$name;
+// echo $str;
+
+// $repeat=str_repeat('*', 5);
+
+// echo $repeat;
+// echo trim(" Hello ");
+$str="HI THERE";
+// echo strtolower($str);
+
+// echo substr($str, 1,
+// 3);
+
+echo strrev($str);
